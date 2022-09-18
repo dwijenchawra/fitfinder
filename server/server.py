e258b89bc2bfe5154c1a474fd17a8d7c59bdd8c0
Pkama07 <pradyun774@gmail.com>
2022-09-18 04:01:01 +0000

    changed server.py for s3 upload

--- server/server.py
+++ server/server.py
@@ -1,23 +1,17 @@
 import json
 from flask import Flask, request, jsonify
 from PIL import Image
-from google.cloud import storage
 from botocore.exceptions import ClientError
 import logging
 import boto3
 import os
 
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "neat-dynamo-362802-b8eb099a1f3f.json"
-
 app = Flask(__name__)
-
+s3_client = boto3.client('s3')
 
 def upload_image(file_name, object_name=None):
     if object_name is None:
         object_name = os.path.basename(file_name)
-
-    s3_client = boto3.client('s3')
     try:
         response = s3_client.upload_file(file_name, "helloworld9938", object_name)
     except ClientError as e:
@@ -26,11 +20,8 @@ def upload_image(file_name, object_name=None):
     return True
     
 
-def download_image(blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket("hello_wrdl123")
-    blob = bucket.blob(blob_name)
-    blob.download_to_filename("download.jpeg")
+def download_image(object_name, file_name):
+    s3_client.download_file('helloworld9938', object_name, file_name)
 
 
 @app.route('/process', methods=['POST'])
@@ -53,8 +44,6 @@ def testRoute():
 
 
 if __name__ == '__main__':
-    s3 = boto3.resource('s3',
-         aws_access_key_id=ACCESS_ID,
-         aws_secret_access_key= ACCESS_KEY)
-    upload_image("download.jpeg")
+    download_image("download.jpeg", "testdownload.jpeg")
+    # upload_image("download.jpeg")
     # app.run(debug=True, host="0.0.0.0", port="5000", threaded="True")
