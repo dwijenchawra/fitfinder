0e65fb52aa0ccb33aed1bae9ac97713f23025d7e
Dwijen Chawra <dwchawra@gmail.com>
2022-09-18 04:20:37 -0400

    added dynamodb

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import json
+from turtle import clone
+import uuid
 from flask import Flask, request, jsonify
 from PIL import Image
 from botocore.exceptions import ClientError
@@ -8,12 +10,29 @@ import os
 
 app = Flask(__name__)
 s3_client = boto3.client('s3')
+dynamo_client = boto3.client('dynamodb')
 
-def upload_image(file_name, object_name=None):
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+def upload_image(file_name):
+    #RUN ML INFERENCE
+    #RUN COLOR DETECTION
+
+    dominantColor = None
+    mlCategory = None
+    imID = uuid.uuid4()
+
+    clothingName = str(dominantColor).strip().capitalize 
+    + str(mlCategory).strip().capitalize()
+
+    toBeSent = {
+        "imageID" : imID,
+        "name" : clothingName,
+        "category" : mlCategory,
+        "color" : dominantColor
+    }
+
     try:
-        response = s3_client.upload_file(file_name, "helloworld9938", object_name)
+        response = s3_client.upload_file(file_name, "helloworld9938", imID)
+        response = dynamo_client.put_item(toBeSent)
     except ClientError as e:
         logging.error(e)
         return False
@@ -38,6 +57,7 @@ def process_image():
 def testRoute():
     print(request.json['info'])
     return jsonify("hello")
+
 
 
 
