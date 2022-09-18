a48a3ea0c88195053bd3b932676e2946f88bcfa6
Pkama07 <pradyun774@gmail.com>
2022-09-17 23:38:39 -0400

    some commit

--- server/server.py
+++ server/server.py
@@ -1,21 +1,60 @@
+import json
 from flask import Flask, request, jsonify
-import numpy as np
+from PIL import Image
+from google.cloud import storage
+from botocore.exceptions import ClientError
+import logging
+import boto3
+import os
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "neat-dynamo-362802-b8eb099a1f3f.json"
 
 app = Flask(__name__)
 
+
+def upload_image(file_name, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, "helloworld9938", object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+    
+
+def download_image(blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("hello_wrdl123")
+    blob = bucket.blob(blob_name)
+    blob.download_to_filename("download.jpeg")
+
+
 @app.route('/process', methods=['POST'])
 def process_image():
-    f = request.files['image']
-    print(type(f))
-    return jsonify('hello')
+    if 'image' not in request.files:
+        return jsonify('no image uploaded')
+    image = request.files['image']
+    img = Image.open(image)
+    img.save('output.jpeg')
+    upload_image("output.jpeg")
+    return jsonify('image went through')
 
-
-@app.route('/', methods=['GET'])
-def test():
+@app.route("/testRoute", methods=['POST'])
+def testRoute():
+    print(request.json['info'])
     return jsonify("hello")
 
 
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5000", threaded="True")
+    s3 = boto3.resource('s3',
+         aws_access_key_id=ACCESS_ID,
+         aws_secret_access_key= ACCESS_KEY)
+    upload_image("download.jpeg")
+    # app.run(debug=True, host="0.0.0.0", port="5000", threaded="True")
--- server/test.py
+++ server/test.py
@@ -5,6 +5,5 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://127.0.0.1:5000/process'
-    img = {'image': iio.v2.imread('256x256bb.jpeg')}
-    r = requests.post(url, files=img)
-    print(r)
+    r = requests.post(url, files={'image': open('256x256bb.jpeg', 'rb')})
+    print(r.text)
